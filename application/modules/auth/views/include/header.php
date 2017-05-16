8864e09d16cc9e42f47fd3f4702ef3274cadaa6c
fanaticc <regolithsjk@gmail.com>
2017-05-16 16:14:55 +0545

    CI with HMVC and ion auth with material design

---
+++ application/modules/auth/views/include/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Inventory Billing</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap core CSS     -->
+    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!--  Material Dashboard CSS    -->
+    <link href="<?php echo base_url();?>assets/css/material-dashboard.css" rel="stylesheet"/>
+
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="<?php echo base_url();?>assets/css/demo.css" rel="stylesheet" />
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+
+</head>
---
+++ application/modules/auth/views/include/sidebar.php
@@ -0,0 +1,52 @@
+<div class="sidebar-wrapper">
+    <ul class="nav">
+        <li class="active">
+            <a href="<?php echo base_url();?>">
+                <i class="material-icons">dashboard</i>
+                <p>Dashboard</p>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo base_url();?>auth/examplePages/user">
+                <i class="material-icons">person</i>
+                <p>User Profile</p>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo base_url();?>auth/examplePages/table">
+                <i class="material-icons">content_paste</i>
+                <p>Table List</p>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo base_url();?>auth/examplePages/typography">
+                <i class="material-icons">library_books</i>
+                <p>Typography</p>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo base_url();?>auth/examplePages/icons">
+                <i class="material-icons">bubble_chart</i>
+                <p>Icons</p>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo base_url();?>auth/examplePages/maps">
+                <i class="material-icons">location_on</i>
+                <p>Maps</p>
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo base_url();?>auth/examplePages/notifications">
+                <i class="material-icons text-gray">notifications</i>
+                <p>Notifications</p>
+            </a>
+        </li>
+        <li class="active-pro">
+            <a href="<?php echo base_url();?>auth/examplePages/upgrade">
+                <i class="material-icons">unarchive</i>
+                <p>Upgrade to PRO</p>
+            </a>
+        </li>
+    </ul>
+</div>
